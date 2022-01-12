90804d460f09f15f801c94dea071e5e717f2fede
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-01-12 15:33:51 +0100

    items route

---
+++ src/main/java/com/pulsars/adventure/controller/ItemController.java
@@ -0,0 +1,20 @@
+package com.pulsars.adventure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/items")
+    public List<String> getItems(){
+        return Arrays.asList(
+                "Pioche",
+                "Pot de miel",
+                "Panier en osier"
+        );
+    }
+}
