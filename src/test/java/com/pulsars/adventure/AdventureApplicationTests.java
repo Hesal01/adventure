50903e780813509304cbb1fb1acb86b6c1c1d79f
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-01-12 14:14:27 +0100

    First setup

---
+++ src/test/java/com/pulsars/adventure/AdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsars.adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
