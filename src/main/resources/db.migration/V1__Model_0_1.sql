2ef2c6077c4db63a18a7eefdc0872162800c151b
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-01-13 15:02:16 +0100

    first migration flyway

---
+++ src/main/resources/db.migration/V1__Model_0_1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE films (
+                       id bigint PRIMARY KEY not null,
+                       title       varchar NOT NULL
+);
+
+INSERT INTO films (id, title) values (1, 'film 1');
