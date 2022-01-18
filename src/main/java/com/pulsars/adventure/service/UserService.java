a0c07739ea5839c9ad7eeba0eae59ee93cc9df84
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-01-19 00:02:52 +0100

    test bug spring secu

--- src/main/java/com/pulsars/adventure/service/UserService.java
+++ src/main/java/com/pulsars/adventure/service/UserService.java
@@ -3,40 +3,16 @@ package com.pulsars.adventure.service;
 import com.pulsars.adventure.model.User;
 import com.pulsars.adventure.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 @Service
-public class UserService implements UserDetailsService {
-
+public class UserService  {
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Iterable<User> getUsers(){
         return userRepository.findAll();
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        User user = userRepository.getUserByUserName(userName);
-        if(user == null){
-            throw new UsernameNotFoundException("User name not found in database.");
-        }
-        return org.springframework.security.core.userdetails.User.builder()
-                .passwordEncoder(input -> bCryptPasswordEncoder.encode(input))
-                .username(user.getUserName())
-                .password(user.getPassword())
-                .authorities(user.getRole())
-                .build();
-    }
 }
