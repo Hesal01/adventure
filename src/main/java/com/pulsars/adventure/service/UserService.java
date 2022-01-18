55e902dbb8986ad4b0673d8bc1a72ff238f1b152
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-01-18 21:15:55 +0100

    password encoder bug

--- src/main/java/com/pulsars/adventure/service/UserService.java
+++ src/main/java/com/pulsars/adventure/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,6 +18,9 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Iterable<User> getUsers(){
         return userRepository.findAll();
@@ -29,6 +33,7 @@ public class UserService implements UserDetailsService {
             throw new UsernameNotFoundException("User name not found in database.");
         }
         return org.springframework.security.core.userdetails.User.builder()
+                .passwordEncoder(input -> bCryptPasswordEncoder.encode(input))
                 .username(user.getUserName())
                 .password(user.getPassword())
                 .authorities(user.getRole())
