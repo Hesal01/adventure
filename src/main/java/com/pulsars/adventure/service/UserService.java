9fae438ddc83d2c9b7ea4ad00289791cf09f0e0b
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-01-19 12:15:14 +0100

    add spring security

--- src/main/java/com/pulsars/adventure/service/UserService.java
+++ src/main/java/com/pulsars/adventure/service/UserService.java
@@ -3,16 +3,38 @@ package com.pulsars.adventure.service;
 import com.pulsars.adventure.model.User;
 import com.pulsars.adventure.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService  {
+public class UserService implements UserDetailsService {
+
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public Iterable<User> getUsers(){
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUserName(userName);
+        if(user == null){
+            throw new UsernameNotFoundException("User name not found in database.");
+        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .passwordEncoder(input -> bCryptPasswordEncoder.encode(input))
+                .username(user.getUserName())
+                .password(user.getPassword())
+                .authorities(user.getRole())
+                .build();
     }
 
 }
