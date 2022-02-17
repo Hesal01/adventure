19011ff83fdafa549af97789a8ae7fd1b6b70c52
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-02-18 00:18:29 +0100

    cors policy problem resolve 4

--- src/main/java/com/pulsars/adventure/AdventureApplication.java
+++ src/main/java/com/pulsars/adventure/AdventureApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AdventureApplication {
@@ -18,16 +15,6 @@ public class AdventureApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
 	}
 
 }
