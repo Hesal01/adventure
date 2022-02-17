e74ff4b0c4fd9aaf13555c1ba9b4c5b0e009ae53
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-02-17 19:16:38 +0100

    resolve cors bug

--- src/main/java/com/pulsars/adventure/security/SecurityConfig.java
+++ src/main/java/com/pulsars/adventure/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        http.csrf().disable();
+        http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         //http.authorizeRequests().anyRequest().permitAll();
         http.authorizeRequests().anyRequest().authenticated();
