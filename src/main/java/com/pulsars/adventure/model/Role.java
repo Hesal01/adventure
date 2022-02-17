f3578eec95263d5056df19601529e74f881a6c01
Jawad <jawad@MacBook-Pro-de-Jawad.local>
2022-02-17 09:05:08 +0100

    bug authentication token resolved

---
+++ src/main/java/com/pulsars/adventure/model/Role.java
@@ -0,0 +1,4 @@
+package com.pulsars.adventure.model;
+
+public class Role {
+}
--- src/main/java/com/pulsars/adventure/service/UserService.java
+++ src/main/java/com/pulsars/adventure/service/UserService.java
@@ -2,6 +2,7 @@ package com.pulsars.adventure.service;
 
 import com.pulsars.adventure.model.User;
 import com.pulsars.adventure.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
+@Slf4j
 @Service
 public class UserService implements UserDetailsService {
 
@@ -21,6 +22,12 @@ public class UserService implements UserDetailsService {
 
     public Iterable<User> getUsers(){
         return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        log.info("Saving new user {} to database", user.getUserName());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 
     @Override
